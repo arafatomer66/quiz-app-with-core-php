ad110c03911d74ee4b1477cb9b2937cca32be69d
Arafat <arafatomer66@gmail.com>
2019-05-02 12:03:26 +0600

    database connection done

---
+++ database.php
@@ -0,0 +1,16 @@
+<?php
+$db_host = "localhost";
+$db_name = "quizzer";
+$db_user = "root";
+$db_pass = "";
+
+// Create connection
+$conn = new mysqli($db_host, $db_user, $db_pass,$db_name);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+
+?>
